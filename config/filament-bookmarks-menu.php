55695c978c0e6b117edb23395e85901716f16b6f
Andreas Kviby <andreaskviby@Mac-mini-som-tillhor-Andreas.local>
2022-12-04 17:46:10 +0100

    Migration added to support the storage of menu item, global and user defined

--- config/filament-bookmarks-menu.php
+++ config/filament-bookmarks-menu.php
@@ -3,4 +3,11 @@
 // config for STAFEGROUPAB/FilamentBookmarksMenu
 return [
 
+    // Icons to use for the icon you might add to your pages in
+    // Filament Admin. Also the colors used for active and inactive.
+    'add_inactive_icon' => ['hero-o-heart'],
+    'add_active_icon' => ['hero-s-heart'],
+    'add_inactive_icon_colors' => ['text-white bg-red-600'],
+    'add_active_icon_colors' => ['text-red-600 bg-white'],
+
 ];
--- src/FilamentBookmarksMenuServiceProvider.php
+++ src/FilamentBookmarksMenuServiceProvider.php
@@ -8,18 +8,16 @@ use STAFEGROUPAB\FilamentBookmarksMenu\Commands\FilamentBookmarksMenuCommand;
 
 class FilamentBookmarksMenuServiceProvider extends PackageServiceProvider
 {
+
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
+        // Register the package here
         $package
             ->name('filament-bookmarks-menu')
             ->hasConfigFile()
             ->hasViews()
             ->hasMigration('create_filament-bookmarks-menu_table')
             ->hasCommand(FilamentBookmarksMenuCommand::class);
+
     }
 }
