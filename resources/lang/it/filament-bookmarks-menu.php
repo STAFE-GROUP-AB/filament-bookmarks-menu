fa6d54d6089ad965ccb246649fb82860e6edb30b
Marco Mapelli <41155893+slamservice@users.noreply.github.com>
2023-02-07 09:20:22 +0100

    Italian translation (#4)

---
+++ resources/lang/it/filament-bookmarks-menu.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filament Bookmarks Menu
+    |--------------------------------------------------------------------------
+    |
+    | All translateable strings goes into this file for this plugin
+    |
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Menu labels
+    |--------------------------------------------------------------------------
+    */
+    'label.global' => 'Globale',
+    'label.private' => 'Privato',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+    'nav.label' => 'Preferiti',
+    'nav.icon' => 'heroicon-o-bookmark',
+
+    'resource.label.bookmark' => 'Preferito',
+    'resource.label.bookmarks' => 'Preferiti',
+
+    'resource.form.label' => 'Etichetta',
+    'resource.form.url' => 'Url',
+    'resource.form.target' => 'Target',
+    'resource.form.sort_order' => 'Ordinamento',
+    'resource.form.user' => 'Utente',
+
+    'resource.form.target.self' => 'Self',
+    'resource.form.target.top' => 'Top',
+    'resource.form.target.blank' => 'Blank',
+
+    'resource.form.user.placeholder.label' => 'Choose user',
+    'resource.form.target.placeholder.label' => 'Choose target',
+
+    'resource.table.label' => 'Etichetta',
+    'resource.table.url' => 'Url',
+    'resource.table.target' => 'Target',
+    'resource.table.sort_order' => 'Ordinamento',
+    'resource.table.user' => 'Belongs to user',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notifications
+    |--------------------------------------------------------------------------
+    */
+    'notification.add' => 'Aggiunto con successo ai preferiti!',
+    'notification.remove' => 'Rimosso con successo dai preferiti!',
+    'notification.empty' => 'Non hai nessun preferito, aggiungine uno e riprova!',
+];
