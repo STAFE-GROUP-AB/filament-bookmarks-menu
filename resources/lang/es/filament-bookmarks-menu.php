50e22f9ca341a7e71d4fc4afc717ebb78d031199
Alvaro León Torres <47951932+alvleont@users.noreply.github.com>
2023-06-03 17:51:21 -0500

    Create filament-bookmarks-menu.php (#9)

---
+++ resources/lang/es/filament-bookmarks-menu.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filament Bookmarks Menu
+    |--------------------------------------------------------------------------
+    |
+    | All translateable strings goes into this file for this plugin
+    |
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Menu labels
+    |--------------------------------------------------------------------------
+    */
+    'label.global' => 'Global',
+    'label.private' => 'Privado',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+    'nav.label' => 'Marcadores',
+    'nav.icon' => 'heroicon-o-bookmark',
+
+    'resource.label.bookmark' => 'Marcador',
+    'resource.label.bookmarks' => 'Marcadores',
+
+    'resource.form.label' => 'Etiqueta',
+    'resource.form.url' => 'Url',
+    'resource.form.target' => 'Destino',
+    'resource.form.sort_order' => 'Orden',
+    'resource.form.user' => 'Usuario',
+
+    'resource.form.target.self' => 'Mismo marco (_self)',
+    'resource.form.target.top' => 'Misma ventana (_top)',
+    'resource.form.target.blank' => 'Nueva Ventana (_blank)',
+
+    'resource.form.user.placeholder.label' => 'Seleccionar usuario',
+    'resource.form.target.placeholder.label' => 'Seleccionar destino',
+
+    'resource.table.label' => 'Etiqueta',
+    'resource.table.url' => 'Url',
+    'resource.table.target' => 'Destino',
+    'resource.table.sort_order' => 'Orden',
+    'resource.table.user' => 'Pertenece al usuario',
+
+    'delete.confirm' => '¿Seguro?',
+    /*
+    |--------------------------------------------------------------------------
+    | Notifications
+    |--------------------------------------------------------------------------
+    */
+    'notification.add' => '¡Tu marcador fue agregado correctamente!',
+    'notification.remove' => '¡Tu marcador fue borrado correctamente!',
+    'notification.empty' => 'No tienes marcadores. ¡Agrega uno y vuelve para encontrarlo aquí!',
+];
